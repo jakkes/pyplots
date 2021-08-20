99e47575f1c2cc741c8775b1de6787c40e2fecb7
Jakob Stigenberg <jakob@jakke.se>
2021-08-20 17:13:56 +0200

    init

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plots",
+    version="0.0.1",
+    author="Jakob Stigenberg",
+    description="Wraps matplotlib for easier plotting.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jakkes/plots",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    python_requires=">=3.7",
+    install_requires=["matplotlib~=3.4.3"]
+)
+
+### Publish
+### python3 setup.py sdist bdist_wheel
+### twine upload dist/*
